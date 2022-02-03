7030d8d2f3077ec776a6e19597ed87540a7d7944
Harsha-S2604 <harsha91148@outlook.com>
2022-02-03 13:47:40 +0000

    database url connection

--- config/db/connectDB.go
+++ config/db/connectDB.go
@@ -21,11 +21,11 @@ import (
 
 
 func ConnectDB() (*sql.DB, error) {
-	userName, password, port, dbName := os.Getenv("POSTGRE_DB_USERNAME"), os.Getenv("POSTGRE_DB_PASSWORD"), os.Getenv("POSTGRE_DB_PORT"), os.Getenv("POSTGRE_DB_NAME")
+	// userName, password, port, dbName := os.Getenv("POSTGRE_DB_USERNAME"), os.Getenv("POSTGRE_DB_PASSWORD"), os.Getenv("POSTGRE_DB_PORT"), os.Getenv("POSTGRE_DB_NAME")
 
-	dbConfigs := "user="+userName+" dbname="+dbName+" password="+password+" host=localhost port="+port+" sslmode=disable"
-	// dbURL := os.Getenv("DATABASE_URL")
-	db, dbErr := sql.Open("postgres", dbConfigs)
+	// dbConfigs := "user="+userName+" dbname="+dbName+" password="+password+" host=localhost port="+port+" sslmode=disable"
+	dbURL := os.Getenv("DATABASE_URL")
+	db, dbErr := sql.Open("postgres", dbURL)
 	if dbErr != nil {
 		return nil, dbErr
 	}
